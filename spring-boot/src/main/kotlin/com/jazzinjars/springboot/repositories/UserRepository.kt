0475fa21a7627ce0f409493b576456e1fd19b88c
jazzinjars <hectorardila@gmail.com>
2018-11-12 18:03:14 +0100

    Adding Repository Tests

--- spring-boot/src/main/kotlin/com/jazzinjars/springboot/repositories/UserRepository.kt
+++ spring-boot/src/main/kotlin/com/jazzinjars/springboot/repositories/UserRepository.kt
@@ -2,5 +2,8 @@ package com.jazzinjars.springboot.repositories
 
 import com.jazzinjars.springboot.model.User
 import org.springframework.data.repository.CrudRepository
+import java.util.*
 
-interface UserRepository: CrudRepository<User, Long>
+interface UserRepository: CrudRepository<User, Long> {
+    fun findById(login: String): Optional<User>
+}
--- spring-boot/src/test/kotlin/com/jazzinjars/springboot/repositories/RepositoriesTests.kt
+++ spring-boot/src/test/kotlin/com/jazzinjars/springboot/repositories/RepositoriesTests.kt
@@ -1,5 +1,9 @@
 package com.jazzinjars.springboot.repositories
 
+import com.jazzinjars.springboot.model.Article
+import com.jazzinjars.springboot.model.User
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
@@ -11,5 +15,29 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 class RepositoriesTests(@Autowired val entityManager: TestEntityManager,
                         @Autowired val userRepository: UserRepository,
                         @Autowired val articleRepository: ArticleRepository) {
+
+    @Test
+    fun `When findById then return Article`() {
+        val jazzinjars = User("springjazzinjars", "Jazz", "InJars")
+        entityManager.persist(jazzinjars)
+        val article = Article("Spring Framework 5.0 goes GA", "Dear Spring community ...", "Lorem ipsum", jazzinjars)
+        entityManager.persist(article)
+        entityManager.flush()
+
+        val found = articleRepository.findById(article.id!!)
+
+        assertThat(found.get()).isEqualTo(article)
+    }
+
+    @Test
+    fun `When findById then return User`() {
+        val jazzinjars = User("springjazzinjars", "Jazz", "InJars")
+        entityManager.persist(jazzinjars)
+        entityManager.flush()
+
+        val found = userRepository.findById(jazzinjars.login)
+
+        assertThat(found.get()).isEqualTo(jazzinjars)
+    }
 
 }
