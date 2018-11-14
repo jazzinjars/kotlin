6b309cb5187e9b528446d83c90854871801cb76c
jazzinjars <hectorardila@gmail.com>
2018-11-14 16:49:00 +0100

    Adding new GETs functions to Controller

--- spring-boot/src/main/kotlin/com/jazzinjars/springboot/KotlinApplication.kt
+++ spring-boot/src/main/kotlin/com/jazzinjars/springboot/KotlinApplication.kt
@@ -1,11 +1,14 @@
 package com.jazzinjars.springboot
 
+import com.samskivert.mustache.Mustache
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class KotlinApplication
+class KotlinApplication {
+    @Bean
+    fun mustacheCompiler(loader: Mustache.TemplateLoader?) =
+            Mustache.compiler().escapeHTML(false).withLoader(loader)
 
-fun main(args: Array<String>) {
-    runApplication<KotlinApplication>(*args)
 }
+
