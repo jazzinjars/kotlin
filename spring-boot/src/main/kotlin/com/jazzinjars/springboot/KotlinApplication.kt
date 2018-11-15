ce2439e2a95eaf30fbd7120d657167073f40f3a3
jazzinjars <hectorardila@gmail.com>
2018-11-15 14:50:15 +0100

    IntegrationTest for repositories

--- spring-boot/src/main/kotlin/com/jazzinjars/springboot/KotlinApplication.kt
+++ spring-boot/src/main/kotlin/com/jazzinjars/springboot/KotlinApplication.kt
@@ -1,14 +1,47 @@
 package com.jazzinjars.springboot
 
+import com.jazzinjars.springboot.model.Article
+import com.jazzinjars.springboot.model.User
+import com.jazzinjars.springboot.repositories.ArticleRepository
+import com.jazzinjars.springboot.repositories.UserRepository
 import com.samskivert.mustache.Mustache
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
+@EnableConfigurationProperties(BlogProperties::class)
 class KotlinApplication {
+
     @Bean
     fun mustacheCompiler(loader: Mustache.TemplateLoader?) =
             Mustache.compiler().escapeHTML(false).withLoader(loader)
 
+    @Bean
+    fun databaseInitializer(userRepository: UserRepository,
+                            articleRepository: ArticleRepository) = CommandLineRunner {
+        val smaldini = User("smaldini", "Stéphane", "Maldini")
+        userRepository.save(smaldini)
+        articleRepository.save(Article(
+                "Reactor Bismuth is out",
+                "Lorem ipsum",
+                "dolor **sit** amet https://projectreactor.io/",
+                smaldini,
+                1
+        ))
+        articleRepository.save(Article(
+                "Reactor Aluminium has landed",
+                "Lorem ipsum",
+                "dolor **sit** amet https://projectreactor.io/",
+                smaldini,
+                2
+        ))
+    }
+
+    fun main(args: Array<String>) {
+        runApplication<KotlinApplication>(*args)
+    }
 }
 
